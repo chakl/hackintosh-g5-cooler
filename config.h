169f73e3f7d9798f1a4287a7db93e17b0768bb44
Olaf Schreck <ckl.github@syscall.de>
2020-12-12 22:51:15 +0100

    pushbutton LED not controlled by MCU

--- config.h
+++ config.h
@@ -9,7 +9,6 @@
   #define FRONT_ENV_SENSOR_PIN 7
   #define REAR_ENV_SENSOR_PIN 8
   // status LED pins, if used
-  #define PUSHBUTTON_LED_PIN 2
   //#define STATUS_GREEN_LED_PIN x
   //#define STATUS_RED_LED_PIN x
   // SPI pins, if used (for MCP4162)
@@ -29,7 +28,6 @@
   #define FRONT_ENV_SENSOR_PIN 5      // D1 XXX
   #define REAR_ENV_SENSOR_PIN 5       // D1
   // status LED pins, if used
-  #define PUSHBUTTON_LED_PIN 2
   //#define STATUS_GREEN_LED_PIN x
   //#define STATUS_RED_LED_PIN x
   // SPI pins, if used (for MCP4162)
@@ -73,8 +71,6 @@
 #define REAR_ENV_SENSOR_TYPE DHT22_SENSOR      // type of rear sensor (DHT22 only)
 #define REAR_ENV_SENSOR_READ_INTERVAL 3000     // DHT22 requires min 2 secs between readings
 
-// support Apple G5 front panel pushbutton LED
-#define WITH_PUSHBUTTON_LED
 // support bicolor status LED
 //#define WITH_BICOLOR_STATUS_LED
 
